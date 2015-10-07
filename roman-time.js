bf9f84ca075c3b156b436376c1031d55a18676ff
Vasilii <kuznecov_vasilii@.mail.ru>
2015-10-07 21:35:44 +0500

    табы заменены на 4 пробела

--- roman-time.js
+++ roman-time.js
@@ -1,67 +1,67 @@
 function toRoman (number) {
-  var romanNumber = "";
+    var romanNumber = "";
 
-  if (number == 0) {
-    romanNumber += '-'
+    if (number == 0) {
+        romanNumber += '-';
+        return romanNumber;
+    }
+
+    var numL = parseInt(number / 50);
+    number -= numL*50;
+    romanNumber = appendSymbols(romanNumber, 'L', numL);
+    if (number >= 40) {
+        number -= 40;
+        romanNumber += 'XL';
+    }
+
+
+    var numX = parseInt(number / 10);
+    number -= numX*10;
+    romanNumber = appendSymbols(romanNumber, 'X', numX);
+    if (number%10 == 9) {
+        number -= 9;
+        romanNumber += 'IX';
+    }
+
+    var numV = parseInt(number / 5);
+    number -= numV*5;
+    romanNumber = appendSymbols(romanNumber, 'V', numV);
+    if (number%5 == 4) {
+        number -= 4;
+        romanNumber += 'IV';
+    }
+
+    var numI = number;
+    romanNumber = appendSymbols(romanNumber, 'I', numI);
     return romanNumber;
-  }
-
-  var numL = parseInt(number / 50);
-  number -= numL*50;
-  romanNumber = appendSymbols(romanNumber, 'L', numL);
-  if (number >= 40) {
-    number -= 40;
-    romanNumber += 'XL';
-  }
-
-
-  var numX = parseInt(number / 10);
-  number -= numX*10;
-  romanNumber = appendSymbols(romanNumber, 'X', numX);
-  if (number%10 == 9) {
-    number -= 9;
-    romanNumber += 'IX';
-  }
-
-  var numV = parseInt(number / 5);
-  number -= numV*5;
-  romanNumber = appendSymbols(romanNumber, 'V', numV);
-  if (number%5 == 4) {
-    number -= 4;
-    romanNumber += 'IV';
-  }
-
-  var numI = number;
-  romanNumber = appendSymbols(romanNumber, 'I', numI);
-  return romanNumber;
 }
 
 function appendSymbols (string, symbol, number) {
-    for (var i = 0 ; i < number ; i++) {
-      string += symbol;
-    }
-    return string;
+        for (var i = 0 ; i < number ; i++) {
+            string += symbol;
+        }
+        return string;
 }
 
 function toAsciiGraphics (string) {
-  var asciiGraphics = {
-    'I' : {0:' IIIIII ', 1:'   II   ', 2:'   II   ', 3:'   II   ', 4:' IIIIII '},
-    'V' : {0:' V    V ', 1:' V    V ', 2:' V    V ', 3:'  V  V  ', 4:'   VV   '},
-    'X' : {0:' X    X ', 1:'  X  X  ', 2:'   XX   ', 3:'  X  X  ', 4:' X    X '},
-    'L' : {0:' L      ', 1:' L      ', 2:' L      ', 3:' L    L ', 4:' LLLLLL '},
-    ':' : {0:'   ::   ', 1:'   ::   ', 2:'        ', 3:'   ::   ', 4:'   ::   '},
-    '-' : {0:'        ', 1:'        ', 2:' ------ ', 3:'        ', 4:'        '}
-  };
+    var asciiGraphics = {
+        'I' : {0:' IIIIII ', 1:'   II   ', 2:'   II   ', 3:'   II   ', 4:' IIIIII '},
+        'V' : {0:' V    V ', 1:' V    V ', 2:' V    V ', 3:'  V  V  ', 4:'   VV   '},
+        'X' : {0:' X    X ', 1:'  X  X  ', 2:'   XX   ', 3:'  X  X  ', 4:' X    X '},
+        'L' : {0:' L      ', 1:' L      ', 2:' L      ', 3:' L    L ', 4:' LLLLLL '},
+        ':' : {0:'   ::   ', 1:'   ::   ', 2:'        ', 3:'   ::   ', 4:'   ::   '},
+        '-' : {0:'        ', 1:'        ', 2:' ------ ', 3:'        ', 4:'        '}
+    };
 
-  var asciiString = "";
+    var asciiString = "";
 
-  for (var i = 0 ; i < 5 ; i++) {
-    for (var j = 0; j < string.length; j++) {
-      asciiString += asciiGraphics[string[j]][i];
+    for (var i = 0 ; i < 5 ; i++) {
+        for (var j = 0; j < string.length; j++) {
+            asciiString += asciiGraphics[string[j]][i];
+        }
+        asciiString += '\n';
     }
-    asciiString += '\n';
-  }
-  return asciiString;
+    return asciiString;
 }
 
 var hours = process.argv[2];
@@ -70,18 +70,17 @@ var minutes = process.argv[3];
 var invalidTime = false;
 
 if (parseInt(hours) != hours || parseInt(minutes) != minutes) {
-  invalidTime = true;
+    invalidTime = true;
 }
 
 if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
-  invalidTime = true;
+    invalidTime = true;
 }
 
 if (invalidTime) {
-  console.log('Время указано не верно');
-  return;
+    console.log('Время указано не верно');
+    return;
 }
-
 
 console.log(toAsciiGraphics(toRoman(hours)+':'+toRoman(minutes)));
 return;
